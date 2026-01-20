4d3fdb33a785236a25f9221269f9646c47eac775
PerfTeamBANTAN <terbaiktangerang@gmail.com>
2026-01-20 20:51:25 +0700

    Update monitoring-b2b.js

--- js/monitoring-b2b.js
+++ js/monitoring-b2b.js
@@ -431,11 +431,15 @@ function openTotalDetail(colIndex){
     20: { mode:'DATIN', gaul:'Y' }
   };
 
-  const p = map[colIndex] || {};
   const qs = new URLSearchParams({
-    type: 'total_hi_all_detail',
-    ...p
-  }).toString();
+  type: 'total_hi_all_detail',
+  ...p,
+
+  /* ===== FILTER UI (INI YANG HILANG) ===== */
+  sto:   filterSto?.value   || '',
+  witel: filterWitel?.value || '',
+  hsa:   filterHsa?.value   || ''
+}).toString();
 
   fetch(API_URL + '?' + qs)
     .then(res => res.json())
